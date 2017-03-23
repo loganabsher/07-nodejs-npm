cd71952eb349c25b24789e480bb1b7e3d1f58087
Carlo Santos <carlox7@gmail.com>
2017-03-22 17:05:19 -0700

    finished mvp, finished one stretch goal

--- starter-code/server.js
+++ starter-code/server.js
@@ -7,12 +7,19 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 // TODO: Include all of the static resources as an argument to app.use()
 app.use(express.static('./public'));
-app.get('*', function(request, response) {
+app.get('/', function(request, response) {
+  response.sendFile('public/index.html', {root: '.'});
   // TODO: Using the response object, send the index.html file back to the user
 });
 
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
-
+app.get('/new', function(request, response){
+  response.sendFile('public/new.html', {root: '.'});
+})
+app.use(function(request, response, next){
+  response.status(404).sendFile('404')
+})
 app.listen(PORT, function() {
+  console.log(`Hosted on ${PORT}`);
   // TODO: Log to the console a message that lets you know which port your server has started on
 });
